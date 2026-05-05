8e545ee13ebeea0c3843c0938d6e9ba721b55ee3
azlocaldodo <d.kowolik@primeline.org>
2026-05-05 16:11:45 +0200

    [FIX] sale report: use more specific xpath to find address in t-set block

--- much_qweb_updater_sale/__manifest__.py
+++ much_qweb_updater_sale/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "much. GmbH",
     "website": "https://muchconsulting.de",
     "category": "Technical",
-    "version": "19.0.1.1.3",
+    "version": "19.0.1.1.4",
     "license": "Other proprietary",
     "depends": [
         "base",
