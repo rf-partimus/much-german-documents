bf90542d369f8b9323cc2959d728713ae47402ec
azlocaldodo <d.kowolik@primeline.org>
2026-05-05 17:18:52 +0200

    [FIX] DIN5008 sale report v17 parity: address order, h2 title, metadata labels, footer width
    - Override <t t-set='address'> in l10n_din5008_sale layout: parent name + name + address + phone (BEFORE VAT) + VAT
    - Re-enable h2 'Angebot' heading for sale reports
    - Hide Quotation/Order No. row, replace metadata labels (Angebotsdatum, Ablauf, Verkäufer)
    - Footer: remove table-layout:fixed and adjust column widths so IBAN does not wrap

--- much_qweb_updater_sale/__manifest__.py
+++ much_qweb_updater_sale/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "much. GmbH",
     "website": "https://muchconsulting.de",
     "category": "Technical",
-    "version": "19.0.1.1.5",
+    "version": "19.0.1.1.6",
     "license": "Other proprietary",
     "depends": [
         "base",
